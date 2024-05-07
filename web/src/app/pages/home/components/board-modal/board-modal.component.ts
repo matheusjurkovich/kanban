17ef49788ef13035ce1faff451a78a35102fe43c
Matheus Jurkovich <matheusjurkovich@gmail.com>
2024-05-07 15:50:29 -0300

    feat: added new migration to add onDelete attribute to relation

--- web/src/app/pages/home/components/board-modal/board-modal.component.ts
+++ web/src/app/pages/home/components/board-modal/board-modal.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
+import { Router } from '@angular/router';
 import { ApiServiceService } from 'src/app/core/api/api-service.service';
 import { BoardService } from 'src/app/pages/board/board.service';
 
@@ -14,13 +15,21 @@ export class BoardModalComponent {
   constructor(
     private apiService: ApiServiceService,
     private dialogRef: MatDialogRef<BoardModalComponent>,
-    private boardService: BoardService
+    private boardService: BoardService,
+    private router: Router
   ) {}
 
   addBoard() {
-    this.apiService.addBoard({ title: this.boardName }).subscribe(() => {
-      this.boardService.search$.next();
-      this.dialogRef.close();
+    this.apiService.addBoard({ title: this.boardName }).subscribe({
+      next: (response) => {
+        const boardId = response.id;
+        this.boardService.search$.next();
+        this.router.navigate(['/board', boardId]);
+        this.dialogRef.close();
+      },
+      error: (err) => {
+        console.error('Erro ao adicionar board:', err);
+      },
     });
   }
 }
