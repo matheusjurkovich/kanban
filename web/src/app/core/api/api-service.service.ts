adc3a03af5285324e88bdbe3cebe9867c55c22ec
Matheus Jurkovich <matheusjurkovich@gmail.com>
2024-05-16 14:53:54 -0300

    feat: add environment files

--- web/src/app/core/api/api-service.service.ts
+++ web/src/app/core/api/api-service.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../environments/environment';
 
 export interface Task {
   id: string;
@@ -32,7 +33,7 @@ export interface Board {
   providedIn: 'root',
 })
 export class ApiServiceService {
-  private apiUrl = 'http://localhost:3000';
+  private apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
 
